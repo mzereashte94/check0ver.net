4d6bbe0843027b24367078c6263e3035724deec5
mzereashte94 <mzereashte03@gmail.com>
2026-09-15 21:10:43 +0300

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -2,7 +2,6 @@ import json
 import requests
 
 url = "https://check0ver.net/en/iapps?filter%5BinCategories%5D%5B0%5D=9c60f563-1983-42f0-8882-a26207bd4aaf"
-
 headers = {
     "User-Agent": (
         "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) "
@@ -11,12 +10,13 @@ headers = {
     )
 }
 
-print("Fetching data from CheckOver...")
 response = requests.get(url, headers=headers)
-
 if response.status_code == 200:
+  # لێرەدا دەتوانیت بەشێکی HTML بگەڕێیت بە دوای uuid دا
+  text = response.text
+  # دەتوانیت لە ڕێگەی دەقەکەوە لینکەکان دەربهێنیت
   with open("download_links.txt", "w", encoding="utf-8") as f:
-    f.write(response.text)
-  print("Saved successfully!")
-else:
-  print(f"Failed, status code: {response.status_code}")
+    f.write(
+        "لێرەدا ناوی یارییەکان و لینکەکانیان لە داتای پەڕەکەدا هەن.\n" + text
+    )
+  print("Done!")
